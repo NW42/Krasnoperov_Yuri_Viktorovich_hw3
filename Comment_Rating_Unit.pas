99b89b917270e63a9bd2aa09ff669b7238e613d2
NW42 <nw042@ya.ru>
2020-09-19 13:06:56 +0400

    init commit 3

---
+++ Comment_Rating_Unit.pas
@@ -0,0 +1,36 @@
+unit Comment_Rating_Unit;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    Panel1: TPanel;
+    Panel2: TPanel;
+    Button1: TButton;
+    Memo1: TMemo;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Memo1.Lines.Clear;
+  Memo1.Lines.Add('My new rating');  
+  Memo1.Lines.Add('Comment was added');    
+end;
+
+end.
